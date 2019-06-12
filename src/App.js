19d8295d60c9572d3bc077532c259ec38dff5195
Nathaniel Tomkins <47956058+ntomkins@users.noreply.github.com>
2019-06-12 15:26:34 +0100

    added functions in app for user input and submit (#6)
    * added functions in app for user input and submit
    
    * changed title

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
 import React from 'react';
+import axios from 'axios';
 
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Team SNAMV Awesome Web Crawler</h1>
-      <form>
-        <input type="text" placeholder="URL" name="urlSearchbar" />
-        <input type="submit" name="submitButton" />
-      </form>
-    </div>
-  );
+class App extends React.Component {
+  state = { userInput: '' };
+  render() {
+    return (
+      <div className="App">
+        <h1>Team SNAMV Awesome Web Crawler</h1>
+        <form onSubmit={e => this.hendleSubmit(e)}>
+          <input
+            type="text"
+            placeholder="URL"
+            name="urlSearchbar"
+            onChange={e => this.hendleUserInput(e)}
+          />
+          <input type="submit" name="submitButton" />
+        </form>
+      </div>
+    );
+  }
+  hendleUserInput = e => {
+    this.setState({ userInput: e.target.value });
+  };
+  hendleSubmit = e => {
+    e.preventDefault();
+    // return axios.get('api.com', { url: this.state.userInput });
+  };
 }
 
 export default App;
